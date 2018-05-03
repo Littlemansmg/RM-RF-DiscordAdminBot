3c1a2921d13cf932d02e7e1e65a67042421188c1
LittlemanSMG <littlemansmg@gmail.com>
2018-05-03 17:41:00 -0500

    command on mention test

--- AdminBot.py
+++ AdminBot.py
@@ -47,7 +47,7 @@ async def on_command_error(error, ctx):
 @bot.event
 async def on_message(message):
     if message.content.startswith(bot.user.mention + ' noob'):
-            await bot.invoke(commands.Context)
+            await bot.invoke(message)
 
 
 # format help first.
